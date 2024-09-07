da646cbe34ff828dd1081cf3fdcf587de2156101
Atul65kumar <atulkumar6597@gmail.com>
2024-09-07 17:52:41 +0530

    Math Class Program

---
+++ MathClass/Math_Class.java
@@ -0,0 +1,19 @@
+package MathClass;
+
+public class Math_Class {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Math.addExact(45, 85)); //int
+		System.out.println(Math.addExact(453456l, 56)); //long
+		System.out.println(Math.subtractExact(45, 56));
+		System.out.println(Math.multiplyExact(5, 6));
+		System.out.println(Math.max(453456l, 7777777));
+		System.out.println(Math.min(453456l, 56876));
+		System.out.println(Math.sqrt(81));
+		System.out.println(Math.random());
+		System.out.println(Math.abs(-90));
+		System.out.println(Math.PI);
+	}
+
+}
